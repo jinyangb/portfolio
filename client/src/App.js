7b8a3521099bbaca9767a606ea38e8877abd0c8d
jinyangb <your_email@example.com>
2022-01-27 14:38:37 -0500

    set up react components

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <div>
+      <header></header>
+      <div>
+        <Home />
+      </div>
+    </div>
+  )
+}
+
+export default App
