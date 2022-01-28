ae27fb50ec3dc0a3ce94fab3fd5aa7279bb5c44e
jinyangb <your_email@example.com>
2022-01-27 19:03:49 -0500

    css for headings

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,26 +7,31 @@ const Home = (props) => {
       <div className="greeting">
         <h1>Hi I'm Jin</h1>
       </div>
-      <div className="about">
-        <p>
-          I am a well-rounded full-stack developer with a rich background
-          experience in customer-facing roles and scaling businesses. Through
-          starting my own business, I had to learn many aspects of building a
-          company website to marketing and sales, and eventually became
-          interested in development. One of my favorite parts of being a
-          software engineer is helping a team come together and finding the most
-          effective way to work. With my strong communication skills, I believe
-          in treating all people with utmost respect and kindness.
-        </p>
+      <div>
+        <h2 className="section-heading">About</h2>
+        <div className="about">
+          <p>
+            I am a well-rounded full-stack developer with a rich background
+            experience in customer-facing roles and scaling businesses. Through
+            starting my own business, I had to learn many aspects of building a
+            company website to marketing and sales, and eventually became
+            interested in development. One of my favorite parts of being a
+            software engineer is helping a team come together and finding the
+            most effective way to work. With my strong communication skills, I
+            believe in treating all people with utmost respect and kindness.
+          </p>
+        </div>
       </div>
       <div className="projects">
-        <h2 className="ind-projects">Projects</h2>
-        <div>Project One</div>
-        <div>Project Two</div>
-        <div>Project Three</div>
-        <div>Project Four</div>
+        <h2 className="section-heading">Projects</h2>
+        <div>
+          <div>Project One</div>
+          <div>Project Two</div>
+          <div>Project Three</div>
+          <div>Project Four</div>
+        </div>
       </div>
-      <div>
+      <div className="section-heading">
         <h2>Contact</h2>
       </div>
       <div className="contact-area">
@@ -35,13 +40,13 @@ const Home = (props) => {
           alt="display image"
           className="profile-pic"
         />
-        <ul className="project-list">
+        <ul className="contact-list">
           <li>Email: jinyangb@gmail.com</li>
           <li>GitHub: https://github.com/jinyangb</li>
           <li>LinkedIn: https://www.linkedin.com/in/jinyangb/</li>
         </ul>
       </div>
-      <footer>
+      <footer className="footer-icons">
         <div>
           <a href="https://github.com/jinyangb">
             <img src="https://img.icons8.com/carbon-copy/50/000000/github-squared.png" />
